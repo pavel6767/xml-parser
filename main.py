48e8c828eac241c7c67fb63c5dd508f578294eef
Pavel Machuca <pavelmachuca12@gmail.com>
2020-10-04 00:40:04 -0400

    class accepts additional arg of tag ref

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from lxml import etree
 import json
-tagRef = {
-  'cabin': 'ns:CabinClass'
-}
+
 class ParseXML:
   data = {'cabins': {}}
 
-  def __init__(self, source, target):
-    # self.tagRef = {
-    #   'cabin': tagRef.cabin,
-
-    # }
+  def __init__(self, source, target, tagRef):
+    self.tagRef = {
+      'cabin': tagRef['cabin'],
+      'row': tagRef['row'],
+      'seat': tagRef['seat'],
+      'seatSummary': tagRef['seatSummary'],
+      'features': tagRef['features'],
+      'fee': tagRef['fee']
+    }
     self.tree = etree.parse(source)
     self.target = target
     self.namespaces = {
@@ -21,49 +23,48 @@ class ParseXML:
   def main(self):
     self.buildCabins()
     self.buildSeats()
-    print 'success'
-    # self.saveJson()
+    self.saveJson()
 
   def buildCabins(self):
-    # cabins = self.getPath(tree,'//{self.tagRef[cabin]}'.format(**locals()))
-    cabins = self.getPath(self.tree,'//ns:CabinClass')
+    cabins = self.getPath(self.tree,'//{self.tagRef[cabin]}'.format(**locals()))
     for cabin in cabins:
       # build cabin types
-      cabinType = self.getPath(cabin,'./ns:RowInfo[1]/@CabinType')[0]
+      cabinType = self.getPath(cabin,'./{self.tagRef[row]}[1]/@CabinType'.format(**locals()))[0]
       self.data['cabins'][cabinType] = {
         'layout': cabin.attrib['Layout'],
         'seats': []
       }
+
   def buildSeats(self):
     # build seats
-    seats = self.getPath(self.tree,'//ns:SeatInfo')
+    seats = self.getPath(self.tree,'//{self.tagRef[seat]}'.format(**locals()))
     for seat in seats:
-      summary = self.getPath(seat,'./ns:Summary')
+      summary = self.getPath(seat,'./{self.tagRef[seatSummary]}'.format(**locals()))
 
       # base info
       new_seat = {
         'exitRow': bool(seat.attrib['ExitRowInd'] == 'true'),
-        'available': bool(self.getPath(seat,'./ns:Summary/@AvailableInd')[0] == 'true'),
-        'seatId': self.getPath(seat,'./ns:Summary/@SeatNumber')[0]
+        'available': bool(self.getPath(seat,'./{self.tagRef[seatSummary]}/@AvailableInd'.format(**locals()))[0] == 'true'),
+        'seatId': self.getPath(seat,'./{self.tagRef[seatSummary]}/@SeatNumber'.format(**locals()))[0]
       }
 
       # type
       seat_positions = ["Window", "Center", "Aisle"]
       seatsRegex = "^"+'|'.join(seat_positions)+"$"
-      query = "./ns:Features[re:test(text(), $regex)]/text()"
+      query = "./{self.tagRef[features]}[re:test(text(), $regex)]/text()".format(**locals())
       seatType = self.getPath(seat,query, seatsRegex)
 
       if seatType:
         new_seat['type'] = seatType[0]
       else:
-        new_seat['type'] = self.getPath(seat,'.//ns:Features/@extension')[0]
+        new_seat['type'] = self.getPath(seat,'.//{self.tagRef[features]}/@extension'.format(**locals()))[0]
 
       # price
-      price = self.getPath(seat,'.//ns:Fee/@Amount')
+      price = self.getPath(seat,'.//{self.tagRef[fee]}/@Amount'.format(**locals()))
       if price:
         new_seat['price'] = price[0]
 
-      cabinType = self.getPath(seat,'./parent::ns:RowInfo/@CabinType')[0]
+      cabinType = self.getPath(seat,'./parent::{self.tagRef[row]}/@CabinType'.format(**locals()))[0]
       self.data['cabins'][cabinType]['seats'].append(new_seat)
 
   def getPath(self,element,query, regex = None):
@@ -73,7 +74,18 @@ class ParseXML:
     with open(self.target, 'w') as outfile:
       json.dump(self.data, outfile)
 
-parser = ParseXML('OTA_AirSeatMapRS.xml', 'xpath.json')
+
+
+source = 'OTA_AirSeatMapRS.xml'
+target = 'xpath.json'
+tagRef = {
+  'cabin': 'ns:CabinClass',
+  'row': 'ns:RowInfo',
+  'seat': 'ns:SeatInfo',
+  'seatSummary': 'ns:Summary',
+  'features': 'ns:Features',
+  'fee': 'ns:Fee'
+}
+
+parser = ParseXML(source, target, tagRef)
 parser.main()
-# TODO regex to get all namespaces
-# config class for input
