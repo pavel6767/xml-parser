77423ccec82dbe6e89755862a599ef890b663368
Pavel Machuca <pavelmachuca12@gmail.com>
2020-10-03 23:18:49 -0400

    minimum result with xpath and class

--- main.py
+++ main.py
@@ -1,71 +1,73 @@
-"""
-<ns:CabinClass
-	[x] Type (Seat, Kitchen, Bathroom, etc)
-    - <ns:RowInfo <ns:SeatInfo <ns:Features> text()
-	[x] Seat id (17A, 18A)
-    - <ns:RowInfo <ns:SeatInfo <ns:Summary SeatNumber
-	[x] Seat price
-    - <ns:RowInfo <ns:SeatInfo <ns:Service <ns:Fee Amount
-	[x] Cabin class
-    - <ns:RowInfo CabinType
-	[x] Availability
-    - <ns:RowInfo <ns:SeatInfo <ns:Summary AvailableInd
-  - And any other properties you might find interesting or useful.
-    -
-
-"""
-
 from lxml import etree
 import json
 
 class ParseXML:
   def __init__(self, source, target):
-    self.data = []
+    self.data = {'cabins': {}}
     self.source = source
     self.target = target
 
   def main(self):
     tree = etree.parse(self.source)
-    # refactor to not hardcode ns string
-    cabin = tree.xpath('//ns:CabinClass', namespaces={
-      'ns': 'http://www.opentravel.org/OTA/2003/05/common/'
-      })
-
-    SEATS = {
-      'Aisle': True,
-      'Center': True,
-      'Window': True
+    # TODO modularize this
+    regexpNS = "http://exslt.org/regular-expressions"
+    namespaces = {
+    'ns': 'http://www.opentravel.org/OTA/2003/05/common/',
+    're':regexpNS
     }
 
-    for cabinClass in cabin:
-      new_cabin = []
-      for row in cabinClass:
-        new_row = {
-          'cabinType': row.attrib['CabinType'],
-          'seatLayout': cabinClass.attrib['Layout'],
-          'seats': []}
-        for e in row:
-            if etree.QName(e).localname == 'Characteristics':
-              new_row['rowType'] = e.text
-            else:
-              new_seat = {}
-              for seat_child in e:
-                if etree.QName(seat_child).localname == 'Summary':
-                  new_seat['available'] = bool(seat_child.attrib['AvailableInd'] == 'true')
-                  new_seat['seatId'] = seat_child.attrib['SeatNumber']
-                elif etree.QName(seat_child).localname == 'Features':
-                  if seat_child.text in SEATS.keys():
-                    new_seat['position'] = seat_child.text
-                elif etree.QName(seat_child).localname == 'Service':
-                  new_seat['price'] = int(seat_child[0].attrib['Amount'])
-              new_row['seats'].append(new_seat)
-        new_cabin.append(new_row)
-      self.data.append(new_cabin)
+
+    cabins = tree.xpath('//ns:CabinClass',namespaces=namespaces)
+    for cabin in cabins:
+      # build cabin types
+      cabinType = cabin.xpath('./ns:RowInfo[1]/@CabinType',namespaces=namespaces)[0]
+      self.data['cabins'][cabinType] = {
+        'layout': cabin.attrib['Layout'],
+        'seats': []
+      }
+
+    # build seats
+    seats = tree.xpath('//ns:SeatInfo',namespaces=namespaces)
+    for seat in seats:
+      summary = seat.xpath('./ns:Summary',namespaces=namespaces)
+
+      # base info
+      new_seat = {
+        'exitRow': bool(seat.attrib['ExitRowInd'] == 'true'),
+        'available': bool(seat.xpath('./ns:Summary/@AvailableInd',namespaces=namespaces)[0] == 'true'),
+        'seatId': seat.xpath('./ns:Summary/@SeatNumber',namespaces=namespaces)[0]
+      }
+
+      # type
+      seat_positions = ["Window", "Center", "Aisle"] # extract-var
+      seatsRegex = "^"+'|'.join(seat_positions)+"$"
+      query = "./ns:Features[re:test(text(), $seatsRegex)]/text()"
+      seatType = seat.xpath(query, seatsRegex=seatsRegex,namespaces=namespaces)
+
+      if seatType:
+        new_seat['type'] = seatType[0]
+      else:
+        seatType = new_seat['type'] = seat.xpath('.//ns:Features/@extension',namespaces=namespaces)[0]
+
+
+      # price
+      price = seat.xpath('.//ns:Fee/@Amount',namespaces=namespaces)
+      if price:
+        new_seat['price'] = price[0]
+
+      cabinType = seat.xpath('./parent::ns:RowInfo/@CabinType',namespaces=namespaces)[0]
+      self.data['cabins'][cabinType]['seats'].append(new_seat)
+
     self.saveJson()
+
+  def getPath(self,query):
+    print "hi"
 
   def saveJson(self):
     with open(self.target, 'w') as outfile:
       json.dump(self.data, outfile)
 
-parser = ParseXML('OTA_AirSeatMapRS.xml', 'OTA_AirSeatMapRS.json')
+parser = ParseXML('OTA_AirSeatMapRS.xml', 'xpath.json')
 parser.main()
+# TODO regex to get all namespaces
+# config class for input
